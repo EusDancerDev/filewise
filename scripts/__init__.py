f6c313b76143fe1bd9927cff96f55594f2a54ee8
climatechange-tech <jagabantxo@gmail.com>
2025-04-23 17:21:57 +0200

    Improve sub-package import structure
    - Replace direct imports with `__all__` definition:
      from . import bulk_rename
      from . import compress_pdf
      from . import copy_compress
      ...
      ↓
      __all__ = [
          `bulk_rename`,
          `compress_pdf`,
          `copy_compress`,
          ...
      ]
    
    This change aligns the `scripts` sub-package with the parent package´s
    import philosophy, providing better control over exported symbols
    when using `from filewise.scripts import *`. The public API remains
    unchanged, but is now more explicitly defined.

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,11 +3,13 @@
 
 # filewise/scripts/__init__.py
 
-from . import __init__
-from . import bulk_rename
-from . import compress_pdf
-from . import copy_compress
-from . import eml2pdf_exec
-from . import modify_properties
-from . import msg2pdf_exec
-from . import tweak_pdf
+# Define what should be available when using 'from filewise.scripts import *'
+__all__ = [
+    "bulk_rename",
+    "compress_pdf",
+    "copy_compress",
+    "eml2pdf_exec",
+    "modify_properties",
+    "msg2pdf_exec",
+    "tweak_pdf"
+]
